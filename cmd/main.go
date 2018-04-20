391061e4bafeae8bd3341d3668370d94ffa2e812
Alain Pannetrat <l1l1@gmx.com>
2018-04-21 02:16:01 +0300

    added sample main.go

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/omzlo/goblynk"
+	"math/rand"
+	"os"
+)
+
+func main() {
+	temp := 25
+
+	key := os.Getenv("BLYNK_AUTH_TOKEN")
+	if key == "" {
+		fmt.Println("Missing BLYNK_AUTH_TOKEN environement variable")
+		os.Exit(-2)
+	}
+
+	client := blynk.NewClient(blynk.BLYNK_ADDRESS, key)
+
+	client.RegisterDeviceReaderFunction(2, func(pin uint, body *blynk.Body) {
+		temp = temp + rand.Intn(3) - 1
+		body.PushInt(temp)
+	})
+
+	client.Run()
+}
